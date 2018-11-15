4a5576280fc4925ed103f557a25c4216c0e3aa6b
Lucia Fuentes <maluciafuentes@gmail.com>
2018-11-15 17:13:16 +0100

    Initial Commit

---
+++ assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment1
+//
+//  Created by Lucia  Fuentes on 11/15/18.
+//  Copyright © 2018 Lucia Fuentes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
